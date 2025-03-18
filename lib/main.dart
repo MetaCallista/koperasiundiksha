81789e0d50512766e3d5dbc93fdbafa252f5df0b
MetaCallista <meta.callista@student.undiksha.ac.id>
2025-03-18 19:49:37 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Koperasi Undiksha',
       initialRoute: '/login', 
       routes: {
